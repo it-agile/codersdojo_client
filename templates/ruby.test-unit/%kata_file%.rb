52a5cd60a0f7fc57ade74c9ff5dc9d54a652a2dd
Stefan <stefan.roock@it-agile.de>
2010-12-29 20:18:07 +0100

    Bugfix: Template names ruby

---
+++ templates/ruby.test-unit/%kata_file%.rb
@@ -0,0 +1,20 @@
+# Adapt the code to your code kata %kata_file%.
+
+require 'test/unit'
+
+class %Kata_file%Test < Test::Unit::TestCase
+
+  def test_foo
+    object_under_test = %Kata_file%.new
+    assert_equal("foo", object_under_test.foo)
+  end
+
+end
+
+class %Kata_file%
+
+  def foo
+    "fixme"
+  end
+
+end
