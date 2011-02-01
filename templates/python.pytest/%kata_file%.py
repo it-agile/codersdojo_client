59717d4a9554f665cd74932177a245a1ffc2edc0
Stefan <stefan.roock@it-agile.de>
2011-02-01 16:53:05 +0100

    Added framework python.pytest

---
+++ templates/python.pytest/%kata_file%.py
@@ -0,0 +1,9 @@
+# Adapt the code to your code kata %kata_file%.
+# Important: Test and production code has to be
+#            completely in this file.
+
+def foo(num):	
+    return 'fixme' 
+
+def test_to_roman():	
+    assert foo == 'foo'
