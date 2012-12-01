33b4b665b1b84406e0a5534faedb122d8a490f47
Andreas Simon <coding@andreas-simon.info>
2012-12-01 17:28:55 +0100

    Applied a little "RSpec magic" to matchers

--- spec/unit/run/scheduler_interrupt_listener_spec.rb
+++ spec/unit/run/scheduler_interrupt_listener_spec.rb
@@ -2,27 +2,32 @@ require 'flote/scheduler'
 require 'run/scheduler_interrupt_listener'
 
 describe SchedulerInterruptListener do
+	CURRENT_PROCESS = 0
 
 	before :each do
-		@view_mock = mock
-		@scheduler = Scheduler.new mock.as_null_object
+		@view_mock = mock('view')
+		runner = mock.as_null_object
+		@scheduler = Scheduler.new runner
 		@scheduler.interrupt_listener = SchedulerInterruptListener.new @view_mock, []
 	end
 
 	it "should have no last action initially" do
-		@scheduler.interrupt_listener.last_action_was_exit?.should == false
-		@scheduler.interrupt_listener.last_action_was_resume?.should == false
+		@scheduler.interrupt_listener.should_not be_last_action_was_exit
+		@scheduler.interrupt_listener.should_not be_last_action_was_resume
 	end
 
+	# fails on MRI 1.9.3p327
+	# working on MRI 1.8.x?
 	it "should exit on Ctrl-C and 'e' (exit)" do
 		@view_mock.should_receive :show_kata_exit_message
 		@view_mock.should_receive(:read_user_input).and_return "e"
 		@view_mock.should_receive :show_kata_upload_hint
 
 	  Thread.new { @scheduler.start }
-	  Process.kill "INT", 0
-	  @scheduler.interrupt_listener.last_action_was_exit?.should == true
-	end	
+	  # Kills my whole X session when debugging it in RubyMine
+	  Process.kill "INT", CURRENT_PROCESS
+	  @scheduler.interrupt_listener.should be_last_action_was_exit
+	end
 
 	it "should execute command when the user pressed Ctrl-C and the command-key of the command" do
 		@command_a_mock = mock
